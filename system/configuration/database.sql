406f3d397eacd8e35a17af926a30b32420907732
Marc Scherer <mail@loewenstall.de>
2015-08-09 15:31:48 +0200

    add some testdata to database file system/configuration/database.sql

--- system/configuration/database.sql
+++ system/configuration/database.sql
@@ -1,7 +1,28 @@
--- Database: `blog`
+-- MySQL dump 10.13  Distrib 5.5.41, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: blog
+-- ------------------------------------------------------
+-- Server version	5.5.41-0ubuntu0.14.04.1
 
--- Table structure for table `model_category`
-CREATE TABLE `model_category` (
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `sys_category`
+--
+
+DROP TABLE IF EXISTS `sys_category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sys_category` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL,
   `description` text NOT NULL,
@@ -10,10 +31,112 @@ CREATE TABLE `model_category` (
   `crdate` datetime NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Table structure for table `model_post`
-CREATE TABLE `model_post` (
+--
+-- Dumping data for table `sys_category`
+--
+
+LOCK TABLES `sys_category` WRITE;
+/*!40000 ALTER TABLE `sys_category` DISABLE KEYS */;
+INSERT INTO `sys_category` VALUES (1,'Standard','',NULL,NULL,'2015-08-06 00:00:00');
+/*!40000 ALTER TABLE `sys_category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sys_comment`
+--
+
+DROP TABLE IF EXISTS `sys_comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sys_comment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `crdate` datetime NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `post` int(11) NOT NULL,
+  `hidden` int(2) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sys_comment`
+--
+
+LOCK TABLES `sys_comment` WRITE;
+/*!40000 ALTER TABLE `sys_comment` DISABLE KEYS */;
+INSERT INTO `sys_comment` VALUES (1,'2015-08-04 12:11:18','mail@loewenstall.de','Marc Scherer','Test comment','Here comes some comment text',2,0),(2,'2015-08-06 20:42:06','me@who.com','Someone','Awsome','This is my unreal comment',2,0);
+/*!40000 ALTER TABLE `sys_comment` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sys_files`
+--
+
+DROP TABLE IF EXISTS `sys_files`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sys_files` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `path` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `mime` text NOT NULL,
+  `size` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  KEY `path` (`path`,`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sys_files`
+--
+
+LOCK TABLES `sys_files` WRITE;
+/*!40000 ALTER TABLE `sys_files` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sys_files` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sys_plugin`
+--
+
+DROP TABLE IF EXISTS `sys_plugin`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sys_plugin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `active` int(2) NOT NULL,
+  `settings` text NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  KEY `title` (`title`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sys_plugin`
+--
+
+LOCK TABLES `sys_plugin` WRITE;
+/*!40000 ALTER TABLE `sys_plugin` DISABLE KEYS */;
+/*!40000 ALTER TABLE `sys_plugin` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sys_post`
+--
+
+DROP TABLE IF EXISTS `sys_post`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sys_post` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `category` int(11) DEFAULT NULL,
   `crdate` datetime NOT NULL,
@@ -26,35 +149,30 @@ CREATE TABLE `model_post` (
   `urltitle` varchar(255) NOT NULL,
   `modified` datetime DEFAULT NULL,
   `defaultimage` tinyint(4) DEFAULT NULL,
+  `hidden` int(2) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`),
   KEY `title` (`title`,`urltitle`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Table structure for table `sys_files`
-CREATE TABLE `sys_files` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `path` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `mime` text NOT NULL,
-  `size` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`),
-  KEY `path` (`path`,`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+--
+-- Dumping data for table `sys_post`
+--
 
--- Table structure for table `sys_plugin`
-CREATE TABLE `sys_plugin` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) NOT NULL,
-  `active` int(2) NOT NULL,
-  `settings` text NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id` (`id`),
-  KEY `title` (`title`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+LOCK TABLES `sys_post` WRITE;
+/*!40000 ALTER TABLE `sys_post` DISABLE KEYS */;
+INSERT INTO `sys_post` VALUES (2,1,'2015-08-06 00:00:00','post',1,NULL,'Der erste Post','Hier folgt ein Blindtext zum Ausfüllen des Teaser Text.','<p>Lorem ipsum dolor sit amet.</p>','der-erste-post','2015-08-06 00:00:00',NULL,0),(3,1,'2015-08-08 00:00:00','post',1,NULL,'Noch ein Test','Lorem ipsum','<p>Das ist der eigentliche Blog Text, hier kann nach Belieben viel stehen und ein RTE wird noch eingefügt, um die eingegebenen Texte zu formatieren.</p>','noch-ein-test','2015-08-08 00:00:00',NULL,1);
+/*!40000 ALTER TABLE `sys_post` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
 -- Table structure for table `sys_user`
+--
+
+DROP TABLE IF EXISTS `sys_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `sys_user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
@@ -69,4 +187,26 @@ CREATE TABLE `sys_user` (
   UNIQUE KEY `id` (`id`),
   UNIQUE KEY `name_2` (`name`),
   KEY `name` (`name`,`fullname`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sys_user`
+--
+
+LOCK TABLES `sys_user` WRITE;
+/*!40000 ALTER TABLE `sys_user` DISABLE KEYS */;
+INSERT INTO `sys_user` VALUES (1,'admin','$2a$06$5f4dcc3b5aa765d61d832unfpXPbyRnQv16toxjYPpsujX.xVdLYy','Admin Istrator',1,'mail@domain.com','','2015-08-06 00:00:00','');
+/*!40000 ALTER TABLE `sys_user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2015-08-09 15:30:46
