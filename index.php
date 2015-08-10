3f48ab1b86e592f3a41d9e1b086053feff0ab8eb
Marc Scherer <mail@loewenstall.de>
2015-08-10 23:17:08 +0200

    add some todos and ideas

--- index.php
+++ index.php
@@ -3,16 +3,25 @@
  * BLOG
  * alpha still under development
  *
+ * @todo implement translation
  * @todo implement plugins
  * @todo implement categories
  * @todo implement pages
  * @todo implement user
+ * @todo extend existing cache (for minified code, script)
  *
  * @todo implement blog settings
  * @todo implement user settings
  *
  * @todo implement be/fe pager
  * @todo implement be/fe registration
+ *
+ * @todo create a backend theme
+ *
+ * ideas
+ * - include css/js bootstrap
+ * - implement css/js minify
+ * - include sass
  *
  */
 
